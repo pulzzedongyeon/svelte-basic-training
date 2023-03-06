35a7f5003240d9cd47232b8a2385ce763912aba0
pulzzedongyeon <dongyeon@interactor.com>
2023-03-06 14:40:08 +0900

    First Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	import Counter from './Counter.svelte';
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+</style>
